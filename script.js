4a080db6da76b7f233e003f768b669171416df3f
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 02:52:39 +1100

    destringify?

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const { spawn } = require('child_process');
 
 const transmit = spawn('python3', ['transmit.py']);
 transmit.stdout.on('data', function(data) {
-    
     // Log to debug
-    console.log(data);
+    console.log(JSON.stringify(data));
 });
