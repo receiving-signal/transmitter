05554077e9718258b549699745f96e5527ba3496
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 03:06:46 +1100

    revert

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-import { BSON, ObjectId } from 'bson';
-
 const { spawn } = require('child_process');
+
 const transmit = spawn('python3', ['transmit.py']);
 transmit.stdout.on('data', function(data) {
-    // Log to debug
-    d1 = BSON.serialize(data);
-    d2 = BSON.deserialize(d1);
-    console.log(d2);
+    console.log(parse(data));
 });
