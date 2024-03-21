0829cdfa3fa539dea49343aa061418aecb1b5439
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 02:57:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const { spawn } = require('child_process');
+import { BSON, ObjectId } from 'bson';
 
+const { spawn } = require('child_process');
 const transmit = spawn('python3', ['transmit.py']);
 transmit.stdout.on('data', function(data) {
     // Log to debug
-    d1 = JSON.stringify(data);
-    d2 = JSON.parse(d1);
+    d1 = BSON.serialize(data);
+    d2 = BSON.deserialize(d1);
     console.log(d2);
 });
