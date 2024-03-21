3adf98eb83f974f017fc1dadc194171cd63e5c07
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 02:46:31 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const { spawn } = require('child_process');
+const words = []; // Store readings
 
 const transmit = spawn('python3', ['transmit.py']);
 transmit.stdout.on('data', function(data) {
     
+    // Coerce Buffer object to Float
+    words.push(data);
+
     // Log to debug
-    console.log(data);
+    console.log(words);
 });
