b5dffc1a5152595fc8241399dff314dfb03ea459
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 03:23:36 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const { spawn } = require('child_process');
-
-const transmit = spawn('python3', ['transmit.py']);
-transmit.stdout.on('data', function(data) {
-    console.log(data);
-});
+window.onload = function() {
+    const { spawn } = require('child_process');
+    const transmit = spawn('python3', ['transmit.py']);
+    transmit.stdout.on('data', function(data) {
+        document.getElementById('current-time').value = data;
+    });
+};
