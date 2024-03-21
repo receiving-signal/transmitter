aa0036e5ee0d045793b500aed393950927b7698c
LaboratoryManiac <writeleft1@gmail.com>
2024-03-22 02:46:58 +1100

    Revert "Update script.js"
    This reverts commit 3adf98eb83f974f017fc1dadc194171cd63e5c07.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const { spawn } = require('child_process');
-const words = []; // Store readings
 
 const transmit = spawn('python3', ['transmit.py']);
 transmit.stdout.on('data', function(data) {
     
-    // Coerce Buffer object to Float
-    words.push(data);
-
     // Log to debug
-    console.log(words);
+    console.log(data);
 });
